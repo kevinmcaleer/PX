58df08d5a06eaf53604d8a28c5219675bb592058
Kevin McAleer <kevin.mcaleer@advicefactory.co.uk>
2015-09-30 13:43:39 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>PX</title>
+</head>
+<body>
+Welcome to 
+<?php 
+# px start page - index.php 
+# created 30 September 2015
+# created by Kevin McAleer
+# purpose - 
+
+$name = $_SERVER['SCRIPT_NAME'];
+echo 'PX v0.1';
+echo '<br />';
+echo 'Projects List';
+
+?>
+
+</body>
+</html>
