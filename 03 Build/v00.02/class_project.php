2320154f98ad3233bac50fbef3b638e1054aa54f
Kevin McAleer <kevin.mcaleer@advicefactory.co.uk>
2016-02-29 00:25:15 +0000

    house keeping

---
+++ 03 Build/v00.02/class_project.php
@@ -0,0 +1,16 @@
+<?php
+
+class project 
+{
+	public $id; 			// primary key
+	public $name; 			// project name
+	public $programme_id;	// programme id
+	
+	// load the specific project
+	public function load($projectid)
+	{
+	
+	}
+	
+}
+?>
